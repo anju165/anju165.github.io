b515e61af1b9722db84b92aa6a68860aafbf6087
anju165 <55377794+anju165@users.noreply.github.com>
2019-12-21 15:53:01 +0530

    Add files via upload

---
+++ assets/javascripts/songs.js
@@ -0,0 +1,63 @@
+var featuredSongs = [{
+    "song_id": 101,
+    "song_name": "Jeene Laga Hoon",
+    "song_singer": "Atif Aslam",
+    "song_poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdJOD5Pt2j2ubmUmQ6ve3YivasvYspv4tl8MGFFiP83t19l9jg",
+    "song_path": "assets/songs/Jeene_Laga_Hoon.mp3"
+},
+{
+    "song_id": 102,
+    "song_name": "Tu Meri",
+    "song_singer": "Vishal Dadlani",
+    "song_poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS7r8cvKyyYPS4trN5OwUcNs-OaZv0-acR8r7s2-eP5XDcyJfMb",
+    "song_path": "assets/songs/Tu_Meri.mp3"
+},
+{
+    "song_id": 103,
+    "song_name": "kanninullil",
+    "song_singer": "Najeem Irshad",
+    "song_poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjgQO_eownePbsc_Ep-OQGao15l6DhDQq7SHxh0eKIGWjdRZYw",
+    "song_path": "assets/songs/Kanninullil.mp3"
+},
+{
+    "song_id": 104,
+    "song_name": "Jeene Laga Hoon",
+    "song_singer": "Atif Aslam",
+    "song_poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdJOD5Pt2j2ubmUmQ6ve3YivasvYspv4tl8MGFFiP83t19l9jg",
+    "song_path": "assets/songs/Jeene_Laga_Hoon.mp3"
+},
+{
+    "song_id": 105,
+    "song_name": "Tu Meri",
+    "song_singer": "Vishal Dadlani",
+    "song_poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS7r8cvKyyYPS4trN5OwUcNs-OaZv0-acR8r7s2-eP5XDcyJfMb",
+    "song_path": "assets/songs/Tu_Meri.mp3"
+},
+{
+    "song_id": 106,
+    "song_name": "kanninullil",
+    "song_singer": "Najeem Irshad",
+    "song_poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjgQO_eownePbsc_Ep-OQGao15l6DhDQq7SHxh0eKIGWjdRZYw",
+    "song_path": "assets/songs/Kanninullil.mp3"
+},
+{
+    "song_id": 107,
+    "song_name": "Jeene Laga Hoon",
+    "song_singer": "Atif Aslam",
+    "song_poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRdJOD5Pt2j2ubmUmQ6ve3YivasvYspv4tl8MGFFiP83t19l9jg",
+    "song_path": "assets/songs/Jeene_Laga_Hoon.mp3"
+},
+{
+    "song_id": 108,
+    "song_name": "Tu Meri",
+    "song_singer": "Vishal Dadlani",
+    "song_poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS7r8cvKyyYPS4trN5OwUcNs-OaZv0-acR8r7s2-eP5XDcyJfMb",
+    "song_path": "assets/songs/Tu_Meri.mp3"
+},
+{
+    "song_id": 109,
+    "song_name": "kanninullil",
+    "song_singer": "Najeem Irshad",
+    "song_poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTjgQO_eownePbsc_Ep-OQGao15l6DhDQq7SHxh0eKIGWjdRZYw",
+    "song_path": "assets/songs/Kanninullil.mp3"
+}]
